653946ce333e2c516eba2e4a5d86f8fa86f653b5
mtb_beta <mtb.toya0403@gmail.com>
2020-11-27 03:03:12 +0900

    sample1, 2にコメント追加

--- sample/sample1_get_all_items.py
+++ sample/sample1_get_all_items.py
@@ -4,15 +4,20 @@ import todoist
 
 TODOIST_API_KEY = config("TODOIST_API_KEY")
 
+
 def main():
+    """
+    全てのタスクを取得するサンプル
+    """
     api = todoist.TodoistAPI(TODOIST_API_KEY)
     api.sync()
 
-    for item in api.state['items']:
+    for item in api.state["items"]:
         # see: item object document.
         # https://developer.todoist.com/sync/v8/#items
 
-        print(item['content'])
+        print(item["content"])
+
 
 if __name__ == "__main__":
     main()
--- sample/sample2_get_all_projects.py
+++ sample/sample2_get_all_projects.py
@@ -4,15 +4,28 @@ import todoist
 
 TODOIST_API_KEY = config("TODOIST_API_KEY")
 
+
 def main():
+    """
+    全てのプロジェクトを表示するサンプル
+    """
     api = todoist.TodoistAPI(TODOIST_API_KEY)
     api.sync()
 
-    for project in api.state['projects']:
+    for project in api.state["projects"]:
         # see: project object document.
         # https://developer.todoist.com/sync/v8/#projects
 
-        print(project['name'])
+        # アーカイブされているものは飛ばす
+        if project["is_archived"]:
+            continue
+
+        # 削除されているものも飛ばす
+        if project["is_deleted"]:
+            continue
+
+        print(project["id"], project["name"])
+
 
 if __name__ == "__main__":
     main()
